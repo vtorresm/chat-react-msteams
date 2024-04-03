91f33e82f8abed6f5ee3883004c9f383b09d03db
Victor Torres <vatorresm88@gmail.com>
2024-04-03 13:22:36 -0500

    Creando el MessageForm

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -29,28 +29,28 @@ const MessageForm = (props) => {
   };
 
   return (
-    <form className="message-form" onSubmit={handleSubmit}>
+    <form className='message-form' onSubmit={handleSubmit}>
       <input
-        className="message-input"
-        placeholder="Send a message..."
+        className='message-input'
+        placeholder='Send a message...'
         value={value}
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
-      <label htmlFor="upload-button">
-        <span className="image-button">
-          <PictureOutlined className="picture-icon" />
+      <label htmlFor='upload-button'>
+        <span className='image-button'>
+          <PictureOutlined className='picture-icon' />
         </span>
       </label>
       <input
-        type="file"
+        type='file'
         multiple={false}
-        id="upload-button"
+        id='upload-button'
         style={{ display: 'none' }}
         onChange={handleUpload.bind(this)}
       />
-      <button type="submit" className="send-button">
-        <SendOutlined className="send-icon" />
+      <button type='submit' className='send-button'>
+        <SendOutlined className='send-icon' />
       </button>
     </form>
   );
