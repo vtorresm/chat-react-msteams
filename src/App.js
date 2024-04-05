7212f4fda621bff504e51a6e8b454f79654ce86d
Victor Torres <vtorremo@emeal.nttdata.com>
2024-04-05 17:22:43 -0500

    Creando el Message Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
   return (
     <ChatEngine
     height='100vh'
-    projectID=''
-    userName=''
-    userSecret=''
+    projectID='8073797b-fe52-4306-b788-d13f6d59c7a4'
+    userName='vtorremo'
+    userSecret='vtorremo'
+    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   )
 }
