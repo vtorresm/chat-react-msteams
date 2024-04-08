0bbff06be7f222abfaf0e62010cb30fdee4ef31b
Victor Torres <vatorresm88@gmail.com>
2024-04-08 13:36:14 -0500

    Termianando el APP.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '8073797b-fe52-4306-b788-d13f6d59c7a4';
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
-    height='100vh'
-    projectID='8073797b-fe52-4306-b788-d13f6d59c7a4'
-    userName='vtorremo'
-    userSecret='vtorremo'
-    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      height='100vh'
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3'
+        ).play()
+      }
     />
-  )
-}
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
